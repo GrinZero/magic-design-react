7a8dea662d5df1beae7d848f4e1791e79fd2c752
bugyalu <774933704@qq.com>
2022-08-24 13:27:30 +0800

    chore: for umi -> github page

--- .umirc.ts
+++ .umirc.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
 	mode: 'doc',
+	base: '/magic-design-react',
+	publicPath: '/magic-design-react/',
+	exportStatic: {},
 	title: 'magic-design-react',
 	favicon: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
 	logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
