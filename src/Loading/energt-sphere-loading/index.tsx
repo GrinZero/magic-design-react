509367054bd72975e3e5dc125460748b661fee52
bugyalu <774933704@qq.com>
2022-09-06 18:12:31 +0800

    docs: docs update;

--- src/Loading/energt-sphere-loading/index.tsx
+++ src/Loading/energt-sphere-loading/index.tsx
@@ -6,12 +6,19 @@ import './index.less';
 interface EnergtSphereProps extends ComponentProps {
 	speed?: string;
 	color?: string;
+	delay?: string;
 }
 
-const EnergtSphereLoading: React.FC<EnergtSphereProps> = ({ className = '', speed = '.5s', color = '#52e1ef' }) => {
+const EnergtSphereLoading: React.FC<EnergtSphereProps> = ({
+	className = '',
+	speed = '.5s',
+	color = '#52e1ef',
+	delay = '0s',
+}) => {
 	const styles: any = {
 		'--speed': speed,
 		'--color': color,
+		'--delay': delay,
 	};
 	return (
 		<div className={`enregt-sphere-loading animate__animated ${className}`} style={styles}>
